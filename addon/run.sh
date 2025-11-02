9325fe56972a79e23f134db0077456d0871211b8
Ger Jan <gerjanvdbosch@live.nl>
2025-11-02 13:22:40 +0100

    Update run.sh

--- addon/run.sh
+++ addon/run.sh
@@ -5,9 +5,7 @@ set -euo pipefail
 bashio::log.info "Preparing to start..."
 
 MODEL_DIR="$(bashio::config 'model_dir' || echo '/homeassistant/data/adaptive_thermostat')"
-MAIN_PY="/app/run_proc.py"
-PYTHON_BIN="python3"
 
 mkdir -p "${MODEL_DIR}"
 
-python3 -u /app/run_proc.py
+python3 -u ./app/run_proc.py
