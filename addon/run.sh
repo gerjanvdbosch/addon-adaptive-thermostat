85beab9549fdbb494b20402584ef09d164d99595
Ger Jan <gerjanvdbosch@live.nl>
2025-11-02 20:29:39 +0100

    Update run.sh

--- addon/run.sh
+++ addon/run.sh
@@ -1,10 +1,5 @@
-#!/command/with-contenv bashio
-# shellcheck shell=bash
-set -euo pipefail
+#!/usr/bin/with-contenv bashio
 
 bashio::log.info "Preparing to start..."
-
-SUPERVISOR_TOKEN="$(bashio::addon.supervisor.token)"
-export SUPERVISOR_TOKEN
 
 python3 -u ./src/run.py
