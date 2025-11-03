73a638a45ac3150563a4271d46504c0a4d7a9ad6
Ger Jan <gerjanvdbosch@live.nl>
2025-11-03 22:04:16 +0100

    Update main.py

--- addon/src/main.py
+++ addon/src/main.py
@@ -11,7 +11,7 @@ from trainer import Trainer
 from inferencer import Inferencer
 from ha_client import HAClient
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
 
