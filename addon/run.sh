dcf6e83248c82fa32b8feb4eb12c3a00fe968f2e
Ger Jan <gerjanvdbosch@live.nl>
2025-11-03 21:17:12 +0100

    Update run.sh

--- addon/run.sh
+++ addon/run.sh
@@ -7,4 +7,14 @@ mkdir -p /config/models
 mkdir -p /config/db
 mkdir -p /config/logs
 
+export CLIMATE_ENTITY="$(bashio::config 'climate_entity')"
+export SAMPLE_INTERVAL_SECONDS="$(bashio::config 'sample_interval_seconds')"
+export PARTIAL_FIT_INTERVAL_SECONDS="$(bashio::config 'partial_fit_interval_seconds')"
+export FULL_RETRAIN_TIME="$(bashio::config 'full_retrain_time')"
+export MIN_SETPOINT="$(bashio::config 'min_setpoint')"
+export MAX_SETPOINT="$(bashio::config 'max_setpoint')"
+export MIN_CHANGE_THRESHOLD="$(bashio::config 'min_change_threshold')"
+export BUFFER_DAYS="$(bashio::config 'buffer_days')"
+export SENSORS="$(bashio::config 'sensors' || echo '{}')"
+
 python3 -u ./src/main.py
