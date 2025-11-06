10d16ef8c294a0bad3335fa5d6bc485b4113ee4e
Ger Jan <gerjanvdbosch@live.nl>
2025-11-06 22:16:29 +0100

    Update utils.py

--- addon/src/utils.py
+++ addon/src/utils.py
@@ -1,2 +1,10 @@
 def round_half(x):
-  return round(x * 2) / 2
+    return round(x * 2) / 2
+
+def safe_float(x):
+    if x is None:
+        return None
+    try:
+        return float(x)
+    except Exception:
+        return None
