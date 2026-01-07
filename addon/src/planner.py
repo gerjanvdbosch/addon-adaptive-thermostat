ade3912c7b6c92c95ac34b7e65c3aaa50a7e106f
gerjanvdbosch <gerjanvdbosch@live.nl>
2026-01-07 22:59:51 +0100

    Refactor

--- addon/src/planner.py
+++ addon/src/planner.py
@@ -27,23 +27,24 @@ class Planner:
         self.context.forecast = forecast
         logger.info(f"[Planner] Status {status}")
 
-        if forecast is not None:
-            logger.info(f"[Planner] Reason {forecast.reason}")
-            logger.info(f"[Planner] PV now {forecast.actual_pv}kW")
-            logger.info(f"[Planner] Load now {forecast.load_now}kW")
-            logger.info(f"[Planner] Forecast now {forecast.energy_now}kW")
-            logger.info(f"[Planner] Forecast best {forecast.energy_best}kW")
-            logger.info(f"[Planner] Opportunity cost {forecast.opportunity_cost}")
-            logger.info(f"[Planner] Confidence {forecast.confidence}")
-            logger.info(f"[Planner] Bias {forecast.current_bias}")
-            logger.info(f"[Planner] Planned start {forecast.planned_start}")
 
-        # Compressor freq gebruiken voor load / power inschatting
+#         if forecast is not None:
+#             logger.info(f"[Planner] Reason {forecast.reason}")
+#             logger.info(f"[Planner] PV now {forecast.actual_pv}kW")
+#             logger.info(f"[Planner] Load now {forecast.load_now}kW")
+#             logger.info(f"[Planner] Forecast now {forecast.energy_now}kW")
+#             logger.info(f"[Planner] Forecast best {forecast.energy_best}kW")
+#             logger.info(f"[Planner] Opportunity cost {forecast.opportunity_cost}")
+#             logger.info(f"[Planner] Confidence {forecast.confidence}")
+#             logger.info(f"[Planner] Bias {forecast.current_bias}")
+#             logger.info(f"[Planner] Planned start {forecast.planned_start}")
 
-        # Planner wanneer de verwarming aan moet als het x tijd warm moet zijn
+# Compressor freq gebruiken voor load / power inschatting
 
-        # In zomer, beste DHW moment plannen
-        # In winter, beste verwarmingsmoment plannen en evt in piek/teruglevering DHW (verwarmen stoppen)
-        # DHW zo laat mogelijk voor deadline plannen
+# Planner wanneer de verwarming aan moet als het x tijd warm moet zijn
 
-        # Als het warm blijft tot x uur, geen verwarming nodig en lager zetten
+# In zomer, beste DHW moment plannen
+# In winter, beste verwarmingsmoment plannen en evt in piek/teruglevering DHW (verwarmen stoppen)
+# DHW zo laat mogelijk voor deadline plannen
+
+# Als het warm blijft tot x uur, geen verwarming nodig en lager zetten
